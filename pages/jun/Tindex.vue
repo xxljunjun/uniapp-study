37d42c88aeeac6673dcab210fec175e843ef7380
xxljunjun <liaojunjun@autel.com>
2021-06-01 18:07:39 +0800

    新增首页淘手游

---
+++ pages/jun/Tindex.vue
@@ -0,0 +1,19 @@
+<template>
+  <view class="Tindex">11111111111111111</view>
+</template>
+
+<script>
+export default {
+  props: {},
+  data() {
+    return {}
+  },
+  methods: {},
+  components: {},
+}
+</script>
+
+<style lang="scss" scoped>
+.Tindex {
+}
+</style>
